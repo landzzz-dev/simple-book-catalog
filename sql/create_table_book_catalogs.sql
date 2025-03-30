a9e9d2b6a0e3e230dc6d2b7a5873fb39f14c7e2e
landzzz.dev <landzzz.dev@gmail.com>
2025-03-30 17:38:15 +0800

    Add sql creating database, table and data query

--- sql/create_table_book_catalogs.sql
+++ sql/create_table_book_catalogs.sql
@@ -1,27 +1,20 @@
-CREATE TABLE book_catalogs (
+-- 1. Create Database
+CREATE DATABASE IF NOT EXISTS august_99_exam;
+
+-- 2. Use the newly created database
+USE august_99_exam;
+
+-- 3. Create Table
+CREATE TABLE IF NOT EXISTS book_catalogs (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Title TEXT(255) NOT NULL,
-    ISBN TEXT(255) NOT NULL,
-    Author TEXT(255) NOT NULL,
-    Publisher TEXT(255) NOT NULL,
+    Title VARCHAR(255) NOT NULL,
+    ISBN VARCHAR(255) NOT NULL,
+    Author VARCHAR(255) NOT NULL,
+    Publisher VARCHAR(255) NOT NULL,
     Year INT(4) NOT NULL,
-    Category TEXT(255) NOT NULL
-)
+    Category VARCHAR(255) NOT NULL
+);
 
--- INSERT INTO book_catalogs (
---     Title,
---     ISBN,
---     Author,
---     Publisher,
---     Year,
---     Category
--- )
-
--- VALUES (
---     'test',
---     'test',
---     'test',
---     'test',
---     2025,
---     'test'
--- )
+-- 4. Insert Data
+INSERT INTO book_catalogs (Title, ISBN, Author, Publisher, Year, Category)
+VALUES ('test', 'test', 'test', 'test', 2025, 'test');
